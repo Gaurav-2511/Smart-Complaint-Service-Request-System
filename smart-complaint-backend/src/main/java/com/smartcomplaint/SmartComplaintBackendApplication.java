59080d288a6fcf8dd81cc45becd35b22b498bf49
Gaurav-2511 <gauravlahamage24@gmail.com>
2026-05-05 00:30:31 +0530

    Initial backend and frontend project setup

---
+++ smart-complaint-backend/src/main/java/com/smartcomplaint/SmartComplaintBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartcomplaint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartComplaintBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartComplaintBackendApplication.class, args);
+	}
+
+}
